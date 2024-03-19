37207c9819519a691f9456671b8a9bfa94cd6716
ThabeloMahada <162126436+ThabeloMahada@users.noreply.github.com>
2024-03-19 14:46:56 +0000

    intial commit

---
+++ ResultsApplication/src/main/java/com/mycompany/resultsapplication/ResultsApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.resultsapplication;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ResultsApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
